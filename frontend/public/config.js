1f62d56c20f33560c4efdecac5f3ce20c3c63005
SamaaMehrez <samaakhaled2643@gmail.com>
2026-09-01 04:55:20 +0300

    Point frontend config at OpenShift Routes

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,7 @@
 window.__ENV__ = {
-  API_BASE_URL: '',
+  AUTH_BASE_URL: "https://auth-samaamehrez6-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  CATALOG_BASE_URL: "https://catalog-samaamehrez6-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  BOOKING_BASE_URL: "https://booking-samaamehrez6-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  AI_BASE_URL: "https://ai-insight-samaamehrez6-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  ANALYTICS_BASE_URL: "https://analytics-samaamehrez6-dev.apps.rm2.thpm.p1.openshiftapps.com",
 };
